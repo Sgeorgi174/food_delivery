1d3b8c1347cfc6e33caee6e126d4eaa8e77c842e
Georgi <sgeorgi174@gmail.com>
2023-05-08 18:15:43 +0500

    small changes

---
+++ app/app_2.js
@@ -0,0 +1,19 @@
+// НАХОДИМ ВСЕ ЗАГОЛОВКИ ТАБОВ //
+const menuTab = document.querySelectorAll('[data-tab]')
+// НАХОДИМ ВСЕ КОНТЕНТ БОКСЫ //
+const contentBoxes = document.querySelectorAll('[data-tab-content]')
+
+menuTab.forEach(function(item){
+    item.addEventListener('click', function(){        
+
+        contentBoxes.forEach(function(item){
+            item.classList.add('dishes_hidden')
+        })
+        
+        const contentBox = document.querySelector('#' + this.dataset.tab)
+        contentBox.classList.remove('dishes_hidden')
+    })
+    item.addEventListener('click' function(){})
+})
+
+
